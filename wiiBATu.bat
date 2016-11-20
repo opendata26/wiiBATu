278a2c22a050a2f944383fadb5c937b0dee421ba
opendata26 <opendata26git@mail.com>
2016-11-20 11:19:45 +0000

    Update wiiBATu.bat

--- wiiBATu.bat
+++ wiiBATu.bat
@@ -99,7 +99,7 @@ if %preset_choice% == 3 curl --remote-name-all %zipsurl%%conventional%
 if %preset_choice% == 4 curl --remote-name-all %zipsurl%%conventional_dark%
 if %preset_choice% == 5 curl --remote-name-all %zipsurl%%hacker%
 if %preset_choice% == 6 curl --remote-name-all %zipsurl%%hacker_light%
-7za x * -o%~dp0\for_sd\wiiu
+7za x *.zip -o%~dp0\for_sd\wiiu
 echo put everything in the for_sd folder on your sdcard
 pause
 goto start
